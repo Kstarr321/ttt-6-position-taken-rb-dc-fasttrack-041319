50a1ee62c7cdb672c7b346a8a1332abbfbf68475
Kolton Starr <starrkolton@gmail.com>
2019-04-14 15:03:47 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,11 +2,13 @@
 board = [" "," "," "," "," "," "," "," "," "]
 def position_taken?(board, index)
   if (board[index] == " "|| board[index] == ""|| board[index] == nil)
+    puts "not taken"
     return false
   elsif (board[index] == "X"|| board[index] =="O")
+    puts "taken"
     return true
   end
 end
 
 
-position_taken?(board, 4)
+position_taken?(board,4)
